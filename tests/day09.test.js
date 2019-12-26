d9c56e024c268b2c6427fc9af07f2015cd2e5916
Serge Pirogov <d3ljke@gmail.com>
2019-12-26 11:46:45 +0100

    Update npm

--- tests/day09.test.js
+++ tests/day09.test.js
@@ -9,10 +9,10 @@ test('day 9-1 produces a copy of itself as output', () => {
 
 test('day 9-1 should output a 16-digit number', () => {
   const program = [1102, 34915192, 34915192, 7, 4, 7, 99, 0];
-  expect(day09.getOutputs(program).shift()).toBe(1219070632396864);
+  expect(day09.getOutputs(program)).toBe(1219070632396864);
 });
 
 test('day 9-1 should output the large number in the middle', () => {
   const program = [104, 1125899906842624, 99];
-  expect(day09.getOutputs(program).shift()).toBe(1125899906842624);
+  expect(day09.getOutputs(program)).toBe(1125899906842624);
 });
